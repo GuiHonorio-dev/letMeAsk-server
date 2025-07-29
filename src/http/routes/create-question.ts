63549989673875a6bb7366d5f39f7ed246cf009e
Guilherme Honorio <guileandro006@gmail.com>
2025-07-29 10:41:35 -0300

    Audio caption config

--- src/http/routes/create-question.ts
+++ src/http/routes/create-question.ts
@@ -2,6 +2,8 @@ import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
 import { z } from 'zod'
 import { db } from "../../db/connection.ts";
 import { schema } from "../../db/schema/index.ts";
+import { generateEmbeddings } from "../../services/gemini.ts";
+import { and, eq, sql } from "drizzle-orm";
 
 export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
   app.post("/rooms/:roomId/questions",{
@@ -16,6 +18,22 @@ export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
   }, async (request, reply ) => {
     const { roomId } = request.params
     const { question } = request.body
+
+    const embeddings = generateEmbeddings(question)
+    
+    const chunks = await db
+    .select({
+      id: schema.audioChunks.id,
+      transcription: schema.audioChunks.transcription,
+      similarity: sql<number>`1-(${schema.audioChunks.embeddings} <=> ${embeddings})`
+    })
+    .from(schema.audioChunks)
+    .where(and(
+      eq(schema.audioChunks.roomId, roomId),
+      sql`1 - (${schema.audioChunks.embeddings} <=> ${embeddings}) > 0.7`
+    ))
+    .orderBy(sql`1 - (${schema.audioChunks.embeddings} <=> ${embeddings}) > 0.7`)
+    .limit(3)
   
 
     const result = await db
@@ -37,6 +55,6 @@ export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
 
     return reply.status(201).send({ questionId: insertedQuestion.id })
 
-
+    // Parei em 34:41
   });
 };
